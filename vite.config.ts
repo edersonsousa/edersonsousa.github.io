f3e6921beb71ec7baded9335ea4e7a2bdd0fcbc8
edersonsousa <ederson.sousadasilva@gmail.com>
2023-07-31 12:51:00 -0300

    vite. root e resolve

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/edersonsousa.github.io",
+  root: './src', // Defina o diretório raiz do seu aplicativo, se estiver em um diretório diferente
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'], // Defina as extensões de arquivo suportadas
+  },
 })
