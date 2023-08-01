5a2e6358f1f9bf313e96e1428dd13cf910d3ac1c
edersonsousa <ederson.sousadasilva@gmail.com>
2023-08-01 19:34:13 -0300

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/edersonsousa.github.io",
+  base: "/edersonsousa.github.io/",
   //root: './src', // Defina o diretório raiz do seu aplicativo, se estiver em um diretório diferente
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'], // Defina as extensões de arquivo suportadas
